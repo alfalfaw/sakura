4437a54ec46d6b53ed358d61aa6cfa060c2ff5aa
liujianxiong <liujianxiong@hyx.com>
2022-03-29 18:13:20 +0800

    new feat

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,11 +2,14 @@ const sidebarEl = document.querySelector('.sidebar')
 
 const toggleEl = document.querySelector('.menu-btn i')
 const closeEl = document.querySelector('.close-btn i')
+const headerEl = document.querySelector('.site-header')
 toggleEl.addEventListener('click', () => {
   if (sidebarEl.classList.contains('active')) {
     sidebarEl.classList.remove('active')
+
   } else {
     sidebarEl.classList.add('active')
+
   }
 })
 closeEl.addEventListener('click', () => {
