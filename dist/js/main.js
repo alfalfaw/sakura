089ab16d384b91cd6b392d0614acfedf6949f6dd
liujianxiong <liujianxiong@hyx.com>
2022-03-30 16:55:44 +0800

    new feat

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,6 +9,7 @@ toggleEl.addEventListener('click', () => {
 
   } else {
     sidebarEl.classList.add('active')
+    headerEl.classList.remove('active')
 
   }
 })
@@ -16,3 +17,38 @@ closeEl.addEventListener('click', () => {
   sidebarEl.classList.remove('active')
 
 })
+
+
+let ticking = false;
+let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop
+
+function onPageScroll (scroll_pos, scroll_ori) {
+  // 根据滚动位置做的事
+  console.log(scroll_pos, scroll_ori);
+  const width = document.body.clientWidth
+  if (scroll_ori === 'down' && !sidebarEl.classList.contains('active')) {
+    headerEl.classList.add('active')
+  }
+  if (scroll_pos === 0) {
+    headerEl.classList.remove('active')
+  }
+}
+
+window.addEventListener('scroll', function (e) {
+  const st = window.pageYOffset || document.documentElement.scrollTop
+  let direction = ''
+  if (st > lastScrollTop) {
+    direction = 'down'
+  } else {
+    direction = 'up'
+  }
+  lastScrollTop = st <= 0 ? 0 : st
+
+  if (!ticking) {
+    window.requestAnimationFrame(function () {
+      onPageScroll(lastScrollTop, direction);
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
